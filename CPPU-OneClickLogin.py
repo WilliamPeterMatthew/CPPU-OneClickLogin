3138fcd65edb6d801158b206cab2d7055adf7069
ZhangKaiXin <zkx20020420@foxmail.com>
2025-01-02 10:53:52 +0800

    Add files via upload

--- CPPU-OneClickLogin.py
+++ CPPU-OneClickLogin.py
@@ -23,9 +23,18 @@ header={
     'Referer':'http://192.168.9.18/',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
 url = 'http://192.168.9.18/drcom/login?callback=dr1004&DDDDD='+quote(data['DDDDD'])+'&upass='+quote(data['upass'])+'&0MKKey=&R1=0&R3=0&R6=0&para=00&v6ip=&v=6901'
-response = requests.post(url,data=data,headers=header).status_code
-if response == 200:
-    print('已连接（Code:200）')
+response = requests.post(url,data=data,headers=header)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.title.string if soup.title else ''
+    if title == "信息页":
+        print("连接失败，账号或密码不正确")
+        input('请按回车键（Enter）结束。')
+    elif title == "认证成功页":
+        print('已连接（Code:200）')
+    else:
+        print("未知状态，title不是预期的值")
+        input('请按回车键（Enter）结束。')
 else:
-    print('连接失败（Code:{}）'.format(response))
+    print('连接失败（Code:{}）'.format(response.status_code))
     input('请按回车键（Enter）结束。')
